a303ec28d59368df7e4246174a7c9d2b96ab347f
Bishal Swain <bishalswain@acm.org>
2021-09-14 20:05:12 +0530

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-# profile
-Its My Profile. I will be keeping it updated.
-Thank you for visiting.
-
-I will be creating a blog post soon on creating such pages.
---
+++ precache-manifest.b8aa8a9309a71e07c3544c1c2610b312.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfdf5a1b8cad8f50ca9c36392096fbbd",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "5524b98970c557868fae",
+    "url": "/home/static/css/main.82458425.chunk.css"
+  },
+  {
+    "revision": "bfa2ca303b8e3db6ff9e",
+    "url": "/home/static/js/2.7710c963.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.7710c963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5524b98970c557868fae",
+    "url": "/home/static/js/main.98e9a0c4.chunk.js"
+  },
+  {
+    "revision": "a6364fde428a8f141fdf",
+    "url": "/home/static/js/runtime-main.9fca115f.js"
+  },
+  {
+    "revision": "2d9eccae882a5624402636c1c264bc71",
+    "url": "/home/static/media/bishalmain.2d9eccae.jpg"
+  },
+  {
+    "revision": "915a22ebce8bc3765f2db447952d8d35",
+    "url": "/home/static/media/iisc.915a22eb.png"
+  },
+  {
+    "revision": "dcbd3bf7b0642d2108d9f2a17c7d28e9",
+    "url": "/home/static/media/resume.dcbd3bf7.pdf"
+  }
+]);
