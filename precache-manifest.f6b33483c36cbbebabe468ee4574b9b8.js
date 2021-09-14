3e6319f7658d010581378aaf0dee5207b13f4267
Bishal Swain <bishalswain@acm.org>
2021-09-14 20:13:19 +0530

    Updates

---
+++ precache-manifest.f6b33483c36cbbebabe468ee4574b9b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "721650ff4ed25565e573d851acdc17fd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fceda4931a8420e411ed",
+    "url": "/static/css/main.82458425.chunk.css"
+  },
+  {
+    "revision": "bfa2ca303b8e3db6ff9e",
+    "url": "/static/js/2.7710c963.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.7710c963.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fceda4931a8420e411ed",
+    "url": "/static/js/main.3a6c97e9.chunk.js"
+  },
+  {
+    "revision": "4b222818d57808b69c1b",
+    "url": "/static/js/runtime-main.0284da5c.js"
+  },
+  {
+    "revision": "2d9eccae882a5624402636c1c264bc71",
+    "url": "/static/media/bishalmain.2d9eccae.jpg"
+  },
+  {
+    "revision": "915a22ebce8bc3765f2db447952d8d35",
+    "url": "/static/media/iisc.915a22eb.png"
+  },
+  {
+    "revision": "dcbd3bf7b0642d2108d9f2a17c7d28e9",
+    "url": "/static/media/resume.dcbd3bf7.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.b8aa8a9309a71e07c3544c1c2610b312.js"
+  "/precache-manifest.f6b33483c36cbbebabe468ee4574b9b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
